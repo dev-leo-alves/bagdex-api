643155c0bd57805650b81c18ee9b6ad8b85212d8
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-07-08 17:22:23 -0300

    feat: create tier added
    added function and typos to create a tier

--- src/application/repositories/TiersRepository.ts
+++ src/application/repositories/TiersRepository.ts
@@ -1,6 +1,11 @@
 import { Tier } from "../../domain/entities/Tier"
 
+export interface TierCreateData{
+    name: string;
+    url: string;
+}
 export interface TiersRepository{
     findByName(name: string): Promise<Tier | null>;
     findById(id: string): Promise<Tier | null>;
+    create(data: TierCreateData): Promise<Tier | null>;
 }
