fad6af56cb0b146ff27f31257b4441547891bb02
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-07-08 15:02:48 -0300

    fix: removed comparison solid codes

--- src/application/usecases/repositories/ChallengesRepository.ts
+++
@@ -1,5 +0,0 @@
-import { Challenge } from "../../../domain/entities/challenge"
-
-export interface ChallengesRepository{
-    findById(id: string): Promise<Challenge | null>
-}
--- src/application/usecases/repositories/StudentsRepository.ts
+++
@@ -1,5 +0,0 @@
-import { Student } from "../../../domain/entities/student"
-
-export interface StudentsRepository{
-    findById(id: string): Promise<Student | null>
-}
