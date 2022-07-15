8d03bbf2406b6f373121ce75672b77db2abe46fd
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-07-15 17:53:23 -0300

    feat(factories): added find all tiers factory
    factory to provide configs to make controller

---
+++ src/modules/tiers/factories/find-all-tiers-controller-factory.ts
@@ -0,0 +1,12 @@
+import { Controller } from '../../../core/infra/controller'
+import { PrismaTiersRepository } from '../repositories/prisma-tiers-repository'
+import { FindAllTiers } from '../use-cases/find-all-tiers/find-all-tiers-use-case'
+import { FindAllTiersController } from '../controllers/find-all-tiers/find-all-tiers.controller'
+
+export function makeFindAllTiersController(): Controller {
+  const prismaTiersRepository = new PrismaTiersRepository()
+  const findAllTiers = new FindAllTiers(prismaTiersRepository)
+  const findAllTiersController = new FindAllTiersController(findAllTiers)
+
+  return findAllTiersController
+}
