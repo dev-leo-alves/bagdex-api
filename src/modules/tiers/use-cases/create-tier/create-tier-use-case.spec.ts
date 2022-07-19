a03c6552278ef13f4ff6f31fab08f5b4a8406eed
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-07-19 17:53:58 -0300

    test: added in memory tests
    in memory tests use cases

--- src/modules/tiers/use-cases/create-tier/create-tier-use-case.spec.ts
+++ src/modules/tiers/use-cases/create-tier/create-tier-use-case.spec.ts
@@ -5,6 +5,8 @@ import { InMemoryTiersRepository } from "../../repositories/in-memory-tiers-repo
 import { CreateTier } from "./create-tier-use-case"
 import { TiersRepository } from "../../repositories/tiers-repository";
 import { TierAlreadyExistsError } from "./errors/tier-already-exists-erros";
+import { InvalidIdError } from "../../../../core/domain/entities/errors/invalid-id";
+import { InvalidNameError } from "../../../../core/domain/entities/errors/invalid-name";
 
 type SutType = {
     sut: CreateTier,
@@ -12,12 +14,13 @@ type SutType = {
 }
 
 const makeSut = async (): Promise<SutType> => {
+    const id = 1
     const name = 'any_name'
-    const url = 'https://www.validurl.com'
+    const url = 'https://www.validurl.com/1'
     var tiers: Tier[] = []
     const tiersRepository: TiersRepository = new InMemoryTiersRepository(tiers);
     const sut = new CreateTier(tiersRepository)
-    await sut.execute({ name, url })
+    await sut.execute({ id, name, url })
 
     return {sut, tiersRepository}
 }
@@ -26,26 +29,34 @@ const makeSut = async (): Promise<SutType> => {
 describe("Create tier use case", ()=>{
     test("should not create if name already exists", async ()=>{
         const {sut} = await makeSut()
+        const id = 2
         const name = 'any_name'
-        const url = 'https://www.validurl.com'
 
-        const error = await sut.execute({ name, url })
+        const error = await sut.execute({ id, name })
 
         expect(error.value).toEqual(new TierAlreadyExistsError(name))
     })
 
-    test('should not create tier with invalid url', async () => {
-        const name = 'any_name'
-        const url = 'invalid_url'
-        
-        var tiers: Tier[] = []
-        const tiersRepository: TiersRepository = new InMemoryTiersRepository(tiers);
+    test("should not create if name is invalid", async ()=>{
+        const {sut} = await makeSut()
+        const id = 3
+        const name = 'O'
 
-        const sut = new CreateTier(tiersRepository)
 
-        const error = await sut.execute({ name, url })
+        const error = await sut.execute({ id, name })
 
-        expect(error.value).toEqual(new InvalidUrlError(url))
+        expect(error.value).toEqual(new InvalidNameError(name))
+    })
+
+
+    test('should not create tier with invalid id', async () => {
+        const {sut} = await makeSut()
+        const id = 0
+        const name = 'any_name 5'
+
+        const error = await sut.execute({ id, name })
+
+        expect(error.value).toEqual(new InvalidIdError(id))
     })
 
 
