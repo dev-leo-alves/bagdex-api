0bf7c49eb20fc1d9a3b9b47bf2509f55dd11ab8c
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-07-19 17:48:38 -0300

    test: testing refactor to number id only

--- src/core/domain/entity.spec.ts
+++ src/core/domain/entity.spec.ts
@@ -9,36 +9,15 @@ describe('Core Entity', () => {
     expect(entity.id).toBeTruthy()
   })
 
-  test('should use the provided ID if provided as a string', () => {
-    const id = "151201-dez"
+  test('should use the provided ID if provided', () => {
+    const id = 1
     const entity = new CustomEntity({}, id)
 
     expect(entity.id).toEqual(id)
   })
 
-  test('should be able to check equality as a string', () => {
-    const id = "151201-dez"
-    const entityOne = new CustomEntity({}, id)
-    const entityTwo = new CustomEntity({}, id)
-
-    class Another {}
-    
-    expect(entityOne).not.toEqual(null)
-    expect(entityOne).not.toEqual(new Another() as any)
-
-    expect(entityOne).toEqual(entityOne)
-    expect(entityOne).toEqual(entityTwo)
-  })
-
-  test('should use the provided ID if provided as a number', () => {
-    const id = 151201
-    const entity = new CustomEntity({}, id)
-
-    expect(entity.id).toEqual(id)
-  })
-
-  test('should be able to check equality as a number', () => {
-    const id = 151201
+  test('should be able to check equality', () => {
+    const id = 1
     const entityOne = new CustomEntity({}, id)
     const entityTwo = new CustomEntity({}, id)
 
