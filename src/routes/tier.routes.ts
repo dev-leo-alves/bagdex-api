0ae019f245fa09b13eb60763e92cf256304dfa52
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-07-19 18:07:23 -0300

    feat(routes): new route to findAllTiers

--- src/routes/tier.routes.ts
+++ src/routes/tier.routes.ts
@@ -3,10 +3,12 @@ import express from 'express'
 import { adaptRoute } from '../core/infra/adapters/express-route-adapter'
 
 import { makeCreateTierController } from '../modules/tiers/factories/create-tier-controller-factory'
-
+import { makeFindAllTiersController } from '../modules/tiers/factories/find-all-tiers-controller-factory'
 const tiersRouter = express.Router()
 
 
 tiersRouter.post('/', adaptRoute(makeCreateTierController()))
+tiersRouter.get('/', adaptRoute(makeFindAllTiersController()))
+
 
 export { tiersRouter }
