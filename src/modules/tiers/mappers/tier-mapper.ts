ca13d2a292e1413f46ef09c5bc0a6f3da4a3b28e
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-07-19 17:46:15 -0300

    feat(mappers): added raw Id

--- src/modules/tiers/mappers/tier-mapper.ts
+++ src/modules/tiers/mappers/tier-mapper.ts
@@ -2,13 +2,17 @@ import { Tier as PersistenceTier } from '@prisma/client'
 
 import { Name } from '../../../core/domain/entities/name'
 import { Url } from '../../../core/domain/entities/url'
+import { Id } from '../../../core/domain/entities/id'
 import { Tier } from '../domain/entities/tier'
 import { InvalidNameError } from '../../../core/domain/entities/errors/invalid-name'
+import { InvalidIdError } from '../../../core/domain/entities/errors/invalid-id'
 
 export class TierMapper {
   static toDomain(raw: PersistenceTier): Tier {
     const nameOrError = Name.create(raw.name)
     const urlOrError = Url.create(raw.url)
+    const idOrError = Id.create(raw.id)
+
 
     if (nameOrError.isLeft()) {
       throw new InvalidNameError(raw.name)
@@ -18,10 +22,17 @@ export class TierMapper {
       throw new InvalidNameError(raw.url)
     }
 
+    
+    if (idOrError.isLeft()) {
+      throw new InvalidIdError(raw.id)
+    }
+
     const tierOrError = Tier.create(
       {
+        id: idOrError.value,
         name: nameOrError.value,
         url: urlOrError.value,
+
       },
     )
 
@@ -34,6 +45,7 @@ export class TierMapper {
 
   static async toPersistence(tier: Tier) {
     return {
+      id: tier.id,
       name: tier.name.value,
       url: tier.url.value,
     }
