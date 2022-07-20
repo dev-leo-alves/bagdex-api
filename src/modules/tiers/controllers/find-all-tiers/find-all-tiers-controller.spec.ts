97aa68a6164ba6f15cff6c69a47107599875db8e
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-07-20 13:32:29 -0300

    feat: tests with jest environment

--- src/modules/tiers/controllers/find-all-tiers/find-all-tiers-controller.spec.ts
+++ src/modules/tiers/controllers/find-all-tiers/find-all-tiers-controller.spec.ts
@@ -1,80 +1,63 @@
- import { app } from '../../../../app'
+/**
+ * @jest-enviroment ./prisma/prisma-environment-jest
+ */
+
+import { app } from '../../../../app'
  import { prisma } from '../../../../prisma'
- import { CreateTierController } from '../create-tier/create-tier-controller'
- import { CreateTier } from '../../use-cases/create-tier/create-tier-use-case'
- import { Tier } from '../../domain/entities/tier'
- import { InMemoryTiersRepository } from '../../repositories/in-memory-tiers-repository'
- import { TiersRepository } from '../../repositories/tiers-repository'
- 
+ import request from "supertest"
 
- type SutType ={
-  tiersRepository: TiersRepository
-  createTierStub: CreateTier
-  sut: CreateTierController
-}
-
-const makeSut = (): SutType => {
-    var tiers: Tier[] = []
-    const tiersRepository: TiersRepository = new InMemoryTiersRepository(tiers);
-
-    const createTierStub = new CreateTier(tiersRepository)
-    const sut = new CreateTierController(createTierStub)
-
-    return {sut, createTierStub, tiersRepository}
-}
- 
  describe('Get All Tiers (e2e)', () => {
  
-   it('should be able to get all tags', async () => {
-    const {sut, tiersRepository} = makeSut()
- 
-    
-    await sut.handle( {
-        body: {
+  afterAll(async () => {
+    await prisma.$disconnect()
+  })
+
+  it('should be able to get all tags', async () => {
+   
+    await prisma.tier.createMany({
+      data: [
+        {
           id: 1,
           name: 'Tier 01',
           url: 'https://www.validurl/1'
-        }
-      }
-    )
-
-    await sut.handle({
-        body: {
+        },
+        {
           id: 2,
           name: 'Tier 02',
           url: 'https://www.validurl/2'
-        }
-      }
-    )
-
-    await sut.handle( {
-        body: {
+        },
+        {
           id: 3,
           name: 'Tier 03',
           url: 'https://www.validurl/3'
-        }
-      }
-    )
-    const response = await tiersRepository.findAll()
-
-     expect(response).toEqual({
-       body: [
-         expect.objectContaining({
-           id: 1,
-           name: 'Tier 01',
-           url: 'https://www.validurl/1'
-         }),
-         expect.objectContaining({
-           id: 2,
-           name: 'Tier 02',
-           url: 'https://www.validurl/2'
-         }),
-         expect.objectContaining({
-           id: 3,
-           name: 'Tier 03',
-           url: 'https://www.validurl/3'
-         }),
-       ],
-     })
-   })
- })
+        },
+      ],
+    })
+   
+    
+    const response = await request(app)
+    .get('/api/tiers')
+    .send()
+    console.log(response)
+    expect(response.status).toBe(200)
+    expect(response).toEqual({
+      data: [
+        expect.objectContaining({
+          id: 1,
+          name: 'Tier 01',
+          url: 'https://www.validurl/1'
+        }),
+        expect.objectContaining({
+          id: 2,
+          name: 'Tier 02',
+          url: 'https://www.validurl/2'
+        }),
+        expect.objectContaining({
+          id: 3,
+          name: 'Tier 03',
+          url: 'https://www.validurl/3'
+        }),
+      ],
+    })
+  })
+})
