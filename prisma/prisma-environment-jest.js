15f7aee96fe86913fb7375c45be05f247fec89fc
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-07-26 17:05:12 -0300

    refactor(prisma): adjusts to make local tests on PostgreSQL
    New migrations with tier model; Prisma DB Url is Postresql now; prisma jest environment creates
    local schemas to make tests

--- prisma/prisma-environment-jest.js
+++ prisma/prisma-environment-jest.js
@@ -1,34 +1,36 @@
 const NodeEnvironment = require('jest-environment-node').default;
 import { v4 as uuid } from 'uuid';
-import { resolve } from 'path';
 import { Client } from 'pg';
+import { execSync } from 'child_process';
 import { config } from 'dotenv';
+import { resolve } from 'path';
 
 config({
-  path: resolve(__dirname, '..', '.env.test'),
+  path: resolve(__dirname, '..', '.env'),
 });
 
 class CustomEnvironment extends NodeEnvironment {
   constructor(config) {
     super(config);
     this.schema = `code_schema_${uuid()}`;
-    this.connectionString = `${process.env.DATABASE_URL}&schema=${this.schema}`;
+    this.connectionString = `${process.env.DATABASE_URL}?schema=${this.schema}`;
   }
 
   setup() {
     process.env.DATABASE_URL = this.connectionString;
     this.global.process.env.DATABASE_URL = this.connectionString;
+    execSync('prisma migrate dev');
   }
 
-  //   async teardown() {
-  //     const client = new Client({
-  //       connectionString: this.connectionString,
-  //     });
+  async teardown() {
+    const client = new Client({
+      connectionString: this.connectionString,
+    });
 
-  //     await client.connect();
-  //     await client.query(`DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`);
-  //     await client.end();
-  //   }
+    await client.connect();
+    await client.query(`DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`);
+    await client.end();
+  }
 }
 
 module.exports = CustomEnvironment;
