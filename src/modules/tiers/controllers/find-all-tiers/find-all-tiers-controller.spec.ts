d38b374a92b022ad3b69181c1e96823cc908eecf
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-07-27 16:59:11 -0300

    refactor: only mock tests working

--- src/modules/tiers/controllers/find-all-tiers/find-all-tiers-controller.spec.ts
+++ src/modules/tiers/controllers/find-all-tiers/find-all-tiers-controller.spec.ts
@@ -1,47 +1,38 @@
-/**
- * @jest-environment ./prisma/prisma-environment-jest
- */
+import { TiersRepository } from '../../repositories/tiers-repository'
+import { FindAllTiers } from '../../use-cases/find-all-tiers/find-all-tiers-use-case'
+import { FindAllTiersController } from './find-all-tiers-controller'
+import { InMemoryTiersRepository } from '../../repositories/in-memory-tiers-repository'
+import { Tier } from '../../domain/entities/tier'
+import { HttpResponse } from '../../../../core/infra/http/http-response'
 
- import { app } from '../../../../app'
- import { prisma } from '../../../../prisma'
- import request from "supertest"
 
- describe('Get All Tiers (e2e)', () => {
-  afterAll(async () => {
-    await prisma.$disconnect()
-  })
+type SutType ={
+  tiersRepository: TiersRepository
+  findAllTiersStub: FindAllTiers
+  sut: FindAllTiersController
+}
+
+const makeSut = (): SutType => {
+    var tiers: Tier[] = []
+    const tiersRepository: TiersRepository = new InMemoryTiersRepository(tiers);
+
+    const findAllTiersStub = new FindAllTiers(tiersRepository)
+    const sut = new FindAllTiersController(findAllTiersStub)
+
+    return {sut, findAllTiersStub, tiersRepository}
+}
+
+
+ describe('Get All Tiers', () => {
 
   it('should be able to get all tags', async () => {
-   
+    const {sut} = makeSut()
 
-    await prisma.tier.createMany({
-      data: [
-        {
-          id: 1,
-          name: 'Tier 01',
-          url: 'https://www.validurl/1'
-        },
-        {
-          id: 2,
-          name: 'Tier 02',
-          url: 'https://www.validurl/2'
-        },
-        {
-          id: 3,
-          name: 'Tier 03',
-          url: 'https://www.validurl/3'
-        },
-      ],
+    const httpResponse: HttpResponse = await sut.handle()
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      tiers: [],
     })
-   
-    
-    const response = await request(app)
-    .get('/api/tiers')
-    .send()
-    console.log(response.status)
-
-
-    expect(response.status).toBe(200)
-   
   })
+ 
 })
