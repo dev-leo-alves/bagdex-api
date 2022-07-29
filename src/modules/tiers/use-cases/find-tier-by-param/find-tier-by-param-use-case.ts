0e64c139ddaaa92742139c0a294f07b441216dcd
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-07-29 17:04:06 -0300

    feat: param checker

--- src/modules/tiers/use-cases/find-tier-by-param/find-tier-by-param-use-case.ts
+++ src/modules/tiers/use-cases/find-tier-by-param/find-tier-by-param-use-case.ts
@@ -7,7 +7,22 @@ export class FindTierByParam{
     constructor(private tiersRepository: TiersRepository){}
 
     async execute(param: string): Promise<FindTierByParamResponse> {
+
+        function isNumeric(num: any){
+            return !isNaN(num)
+        }
+        
+        const isId = isNumeric(param)
+
+        if(isId){
+            const idParam = parseInt(param)
+            const tier = await this.tiersRepository.findById(idParam)
+            return tier
+           
+        }
+
         const tier = await this.tiersRepository.findByName(param)
         return tier
+        
     }
 }
