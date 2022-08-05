2fbda48e2aa5642ba1c7a77f497fb49be126dedd
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-08-05 11:57:50 -0300

    refactor: moved files

--- src/modules/tiers/use-cases/create-tier/errors/tier-already-exists-erros.ts
+++
@@ -1,8 +0,0 @@
-import { UseCaseError } from '../../../../../core/domain/errors/use-case-error'
-
-export class TierAlreadyExistsError extends Error implements UseCaseError {
-  constructor(name: string) {
-    super(`The tier "${name}" is already registered.`)
-    this.name = 'TierAlreadyExistsError'
-  }
-}
